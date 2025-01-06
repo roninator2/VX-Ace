f89e05f7f3706116af83ef7b34ee254554ffaf17
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 10:08:36 -0500

    Create Block Save Slots.rb

---
+++ Block Save Slots.rb
@@ -0,0 +1,65 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Block Save Slots                       ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║     prevent access to save slots              ║    18 Sep 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║        Block save slots from being used by the player              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Specify the slots to block in the array                          ║
+# ║     Blocked = [1,4]                                                ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 18 Sep 2021 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Save_Block
+  Blocked = [0,1]
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+class Scene_Save < Scene_File
+  def on_savefile_ok
+    super
+    if R2_Save_Block::Blocked.include?(@index)
+      Sound.play_buzzer
+    else
+      if DataManager.save_game(@index)
+        on_save_success
+      else
+        Sound.play_buzzer
+      end
+    end
+  end
+end

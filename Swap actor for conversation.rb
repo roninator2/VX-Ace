2b653901db9579877bca9f5baa62122fdb7c7a15
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 13:28:17 -0500

    Create Swap actor for conversation.rb

---
+++ Swap actor for conversation.rb
@@ -0,0 +1,67 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Actor Swap For Conversation            ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Swap actor positions                        ║    14 Apr 2022     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Allows to change actors to make a specific actor first       ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Set the variable below for this script                           ║
+# ║                                                                    ║
+# ║   Use script call to swap actors                                   ║
+# ║      swap_actor_convo(X)                                           ║
+# ║        X = actor id to be at the front of the party                ║
+# ║   Then use swap_actor_convo($game_variables[X])                    ║
+# ║        X = the variable number used below                          ║
+# ║     to change the actor back to original position                  ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 14 Apr 2022 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Swap_Actor_Conversation
+  Variable = 1
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+class Game_Interpreter
+  def swap_actor_convo(aid)
+    for i in 0..$game_party.members.size - 1
+      swap = i if $game_party.members[i].id == aid
+    end
+    return if swap == 0
+    $game_variables[R2_Swap_Actor_Conversation::Variable] = $game_party.members[0].id
+    $game_party.swap_order(0, swap)
+  end
+end

d0f10f196d195e541547fbe60db1146124b1dc81
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 13:18:47 -0500

    Create Leech Global Save Patch for Picture Gallery.rb

---
+++ Leech Global Save Patch for Picture Gallery.rb
@@ -0,0 +1,65 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Title                                  ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Compatibility patch                         ║    17 Nov 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Pathc for Leech Global Save & Picture Gallery                ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Plug & Play                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 17 Nov 2021 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+class Scene_Title < Scene_Base
+  alias r2_gallery_title  create_command_window
+  def create_command_window
+    r2_gallery_title
+    @command_window.set_handler(:gallery, method(:command_gallery))
+  end
+  def command_gallery
+    close_command_window
+    SceneManager.call(Scene_Gallery)
+  end
+end
+
+class Window_TitleCommand < Window_Command
+  alias r2_command_gallery_title  make_command_list
+  def make_command_list
+    r2_command_gallery_title
+    if $imported[:lglobal_save]
+      LGlobalSave.load
+      add_command("Gallery", :gallery) if $game_switches[R2_Gallery_Title::Global_Switch] == true
+    end
+  end
+end

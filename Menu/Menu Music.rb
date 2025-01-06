2e1b47030edbe931c4177381876e4a9f8827cab3
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 11:18:53 -0500

    Create Menu Music.rb

---
+++ Menu/Menu Music.rb
@@ -0,0 +1,81 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Menu BGM                               ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Play specific music in menu                 ║    30 Sep 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Play music in menu                                           ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Config music file below                                          ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 30 Sep 2021 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Menu_Sound
+  SONG = "Theme1"
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+class Scene_Menu < Scene_MenuBase
+  #--------------------------------------------------------------------------
+  # * Start Processing
+  #--------------------------------------------------------------------------
+  def start
+    super
+    song = RPG::BGM.new(R2_Menu_Sound::SONG, 100, 100)
+    song.play
+    create_command_window
+    create_gold_window
+    create_status_window
+  end
+end
+
+class Scene_Map < Scene_Base
+  def call_menu
+    Sound.play_ok
+    @last_bgm = RPG::BGM.last
+    SceneManager.call(Scene_Menu)
+    RPG::BGM.stop
+    Window_MenuCommand::init_command_position
+  end
+  alias r2_update_map_scene update_scene
+  def update_scene
+    r2_update_map_scene
+    return if SceneManager.scene_is?(Scene_Menu)
+    @last_bgm.play(@last_bgm.pos) if @last_bgm != nil
+    @last_bgm = nil
+  end
+end

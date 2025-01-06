4b8961aebcf59333e9a57efe32fb6ae614aacb01
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 13:20:25 -0500

    Create Player dash through event but not tile event.rb

---
+++ Map/Player dash through event but not tile event.rb
@@ -0,0 +1,60 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Player Dash through Event              ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Allow player to pass through some events    ║    27 Nov 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Player can go through events but not tiles                   ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Plug and play                                                    ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 27 Nov 2021 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+class Game_Player < Game_Character
+  def passable?(x, y, d)
+    x2 = $game_map.round_x_with_direction(x, d)
+    y2 = $game_map.round_y_with_direction(y, d)
+    return false unless $game_map.valid?(x2, y2)
+    return true if @through || debug_through?
+    id = $game_map.event_id_xy(x2,y2)
+    evpass = false
+    if id != 0
+      evpass = $game_map.events[id].tile_id == 0 ? true : false
+    end
+    return true if dash? && evpass && passable?(x2, y2, d)
+    return false unless map_passable?(x, y, d)
+    return false unless map_passable?(x2, y2, reverse_dir(d))
+    return false if collide_with_characters?(x2, y2)
+    return true
+  end
+end

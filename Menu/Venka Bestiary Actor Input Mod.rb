2ebc54fc31fc84e484f0f44543738123fe191066
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 15:39:55 -0500

    Create Venka Bestiary Actor Input Mod.rb

---
+++ Menu/Venka Bestiary Actor Input Mod.rb
@@ -0,0 +1,57 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Venka Bestiary Actor Input Mod         ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║            Alter Display                      ║    07 Sep 2023     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: Venka Bestiary V1.8                                      ║
+# ║           Roninator2 - Venka Bestairy Game Enemy Debuff Mod        ║
+# ║           Venka Bestairy Actor's Input Addon                       ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║               Fix enemy id for bestiray display                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Plug and Play                                                    ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 07 Sep 2023 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+#==============================================================================
+# ■ Scene_Battle
+#==============================================================================
+class Scene_Battle < Scene_Base
+  #----------------------------------------------------------------------------
+  # ○ new method: add_input_text
+  #----------------------------------------------------------------------------
+  alias r2_add_input_for_enemy_text add_input_text
+  def add_input_text(enemy_id)
+    enemy_id = enemy_id.enemy_id if !enemy_id.is_a?(Integer)
+    r2_add_input_for_enemy_text(enemy_id)
+  end
+end

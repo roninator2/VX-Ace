e56ccd30be9f043feaf337d0904e93ab9f651186
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-01-06 13:31:18 -0500

    Create Title Screen web link.rb

---
+++ Title Screen web link.rb
@@ -0,0 +1,74 @@
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Title Links                            ║  Version: 1.00     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║                                               ╠════════════════════╣
+# ║   Add link to title                           ║    28 Apr 2022     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: nil                                                      ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║       Add links to title screen                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   Configure the settings below                                     ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 28 Apr 2022 - Script finished                               ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+module R2_Web_Link_Title_Screen
+  Link_Text = "Web Page"
+  Link = "https://www.google.ca"
+end
+
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║                      End of editable region                        ║
+# ╚════════════════════════════════════════════════════════════════════╝
+
+class Scene_Title < Scene_Base
+  def create_command_window
+    @command_window = Window_TitleCommand.new
+    @command_window.set_handler(:new_game, method(:command_new_game))
+    @command_window.set_handler(:continue, method(:command_continue))
+    @command_window.set_handler(:web_link, method(:command_weblink))
+    @command_window.set_handler(:shutdown, method(:command_shutdown))
+  end
+  def command_weblink
+    system('start', R2_Web_Link_Title_Screen::Link)
+    @command_window.activate
+  end
+end
+
+class Window_TitleCommand < Window_Command
+  def make_command_list
+    add_command(Vocab::new_game, :new_game)
+    add_command(Vocab::continue, :continue, continue_enabled)
+    add_command(R2_Web_Link_Title_Screen::Link_Text, :web_link)
+    add_command(Vocab::shutdown, :shutdown)
+  end
+end

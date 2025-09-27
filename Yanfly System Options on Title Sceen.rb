f29df229d6ea73e22c9b4670fab79605762e0547
roninator2 <50345160+roninator2@users.noreply.github.com>
2025-09-26 23:04:24 -0400

    Update Yanfly System Options on Title Sceen.rb

--- Yanfly System Options on Title Sceen.rb
+++ Yanfly System Options on Title Sceen.rb
@@ -1,33 +1,67 @@
-# ╔═════════════════════════════════════╦════════════════════╗
-# ║ Title: Title Options                ║  Version: 1.00     ║
-# ║ Author: Roninator2                  ║                    ║
-# ╠═════════════════════════════════════╬════════════════════╣
-# ║ Function:                           ║   Date Created     ║
-# ║    Show Options Command             ╠════════════════════╣
-# ║    on title screen                  ║    16 Mar 2021     ║
-# ╚═════════════════════════════════════╩════════════════════╝
-# ╔══════════════════════════════════════════════════════════╗
-# ║ Requires: Yanfly's System Options                        ║
-# ╚══════════════════════════════════════════════════════════╝
-# ╔══════════════════════════════════════════════════════════╗
-# ║ Script allows accessing the options screen               ║
-# ║  before starting the game                                ║
-# ╚══════════════════════════════════════════════════════════╝
-# ╔══════════════════════════════════════════════════════════╗
-# ║ Updates:                                                 ║
-# ║ 1.00 - 19 Mar 2021 - Script finished                     ║
-# ╚══════════════════════════════════════════════════════════╝
-# ╔══════════════════════════════════════════════════════════╗
-# ║ Terms of use:                                            ║
-# ║ Free for all uses in RPG Maker except nudity             ║
-# ╚══════════════════════════════════════════════════════════╝
+# ╔═══════════════════════════════════════════════╦════════════════════╗
+# ║ Title: Title Options                          ║  Version: 1.01     ║
+# ║ Author: Roninator2                            ║                    ║
+# ╠═══════════════════════════════════════════════╬════════════════════╣
+# ║ Function:                                     ║   Date Created     ║
+# ║    Show Options Command                       ╠════════════════════╣
+# ║    on title screen                            ║    16 Mar 2021     ║
+# ╚═══════════════════════════════════════════════╩════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Requires: Yanfly's System Options                                  ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Brief Description:                                                 ║
+# ║   Script allows accessing the options screen                       ║
+# ║   before starting the game                                         ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Instructions:                                                      ║
+# ║   plug and play                                                    ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Updates:                                                           ║
+# ║ 1.00 - 19 Mar 2021 - Script finished                               ║
+# ║ 1.01 - 20 Sep 2022 - Fixed new game bug                            ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Credits and Thanks:                                                ║
+# ║   Roninator2                                                       ║
+# ║                                                                    ║
+# ╚════════════════════════════════════════════════════════════════════╝
+# ╔════════════════════════════════════════════════════════════════════╗
+# ║ Terms of use:                                                      ║
+# ║  Follow the original Authors terms of use where applicable         ║
+# ║    - When not made by me (Roninator2)                              ║
+# ║  Free for all uses in RPG Maker except nudity                      ║
+# ║  Anyone using this script in their project before these terms      ║
+# ║  were changed are allowed to use this script even if it conflicts  ║
+# ║  with these new terms. New terms effective 03 Apr 2024             ║
+# ║  No part of this code can be used with AI programs or tools        ║
+# ║  Credit must be given                                              ║
+# ╚════════════════════════════════════════════════════════════════════╝
 module DataManager
   def self.setup_new_game
+    create_newgame_objects
     $game_party.setup_starting_members
     $game_map.setup($data_system.start_map_id)
     $game_player.moveto($data_system.start_x, $data_system.start_y)
+    $game_party.members.each { |actor| actor.recover_all }
     $game_player.refresh
     Graphics.frame_count = 0
+  end
+  def self.create_newgame_objects
+    $game_temp          = Game_Temp.new
+    $game_timer         = Game_Timer.new
+    $game_message       = Game_Message.new
+    $game_switches      = Game_Switches.new
+    $game_variables     = Game_Variables.new
+    $game_self_switches = Game_SelfSwitches.new
+    $game_actors        = Game_Actors.new
+    $game_party         = Game_Party.new
+    $game_troop         = Game_Troop.new
+    $game_map           = Game_Map.new
+    $game_player        = Game_Player.new
   end
 end
 class Scene_Title < Scene_Base
